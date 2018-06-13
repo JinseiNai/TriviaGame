0030eaf626fc4d7191ee0f716dea3df39529f04a
Calvin <jinseinai@gmail.com>
2018-06-12 18:21:53 -0700

    added folders and questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+// create object to hold game questions and answers
+let game = {
+    questions: [
+        one = {
+            ask: 'What is the Italian word for pie?',
+            correctAnswer: 'Pizza',
+            wrongAnswer: ['Pasta', 'Piano', 'Dutch']
+        },
+        two = {
+            ask: 'What is the national flower of Wales?',
+            correctAnswer: 'Daffodil',
+            wrongAnswer: ['Clover', 'Dahlia', 'Azalea']
+        },
+        three = {
+            ask: 'Which Australian marsupial enjoys eating eucalptus leaves?',
+            correctAnswer: 'Koala',
+            wrongAnswer: ['Kangaroo', 'Dingo', 'Quoll']
+        },
+        four = {
+            ask: 'In nautical terms, what is the opposite of port?',
+            correctAnswer: 'Starboard',
+            wrongAnswer: ['Portside', 'Land ho', 'Plank']
+        },
+        five = {
+            ask: 'Name the port of Rome',
+            correctAnswer: 'Ostia',
+            wrongAnswer: ['Albania', 'Cyprus', 'Romania']
+        },
+        six = {
+            ask: "What was Marilyn Monro's natural hair color?",
+            correctAnswer: 'Ginger',
+            wrongAnswer: ['Brunette', 'Black', 'Blonde']
+        },
+        seven = {
+            ask: 'Which city does the River Lagan flow through?',
+            correctAnswer: 'Belfast',
+            wrongAnswer: ['Beslow', 'Berlin', 'Prague']
+        },
+        eight = {
+            ask: 'What is one quarter of 1,000?',
+            correctAnswer: 250,
+            wrongAnswer: [25, 750, 1250]
+        },
+        nine = {
+            ask: 'How many sides, in total, would three triangles and three rectangles have?',
+            correctAnswer: 21,
+            wrongAnswer: [10, 24, 17]
+        },
+        ten = {
+            ask: 'When did the French Revolution end?',
+            correctAnswer: 1799,
+            wrongAnswer: [1679, 1809, 1899]
+        }
+    ]
+}
