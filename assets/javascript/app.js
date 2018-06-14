84fff2116ca1c6ca432668b17034dd73b82859ee
Calvin <jinseinai@gmail.com>
2018-06-13 17:25:50 -0700

    Displays questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,50 +55,87 @@ let game = {
     // Set variables
     wins: 0,
     losses: 0,
+    currentQuestion: 0,
+    secondsLeft: 8,
+    intervalId: '',
     pickedQuestions: [],
     questionAnswers: [],
 
+    // Create a timer
+    timer: function() {
+        game.intervalId = setInterval(game.count, 1000)
+    },
+
+    count: function() {
+
+    },
+
     // Get questions randomly (no duplicate)
     getQuestions: function () {
-        let qNumber = Math.floor(Math.random() * (game.questions.length));
-        // No duplicates
-        while (game.pickedQuestions.indexOf(qNumber) !== -1) {
-            qNumber = Math.floor(Math.random() * (game.questions.length));
+        for (i = 0; i < 5; i++) {
+            let qNumber = Math.floor(Math.random() * (game.questions.length));
+            // No duplicates
+            while (game.pickedQuestions.indexOf(qNumber) !== -1) {
+                qNumber = Math.floor(Math.random() * (game.questions.length));
+            }
+            game.pickedQuestions.push(qNumber);
+            console.log(qNumber); // Delete Later
         }
-        game.pickedQuestions.push(qNumber);
-        console.log(qNumber); // Delete Later
     },
     // Display the question
     // Display answers
     showQuestions: function () {
+        // Clear current content in DOM
+        $('.main-content').empty();
+        // Clear answers in array
+        game.questionAnswers = [];
         // Create new div to hold question and answers.
         let newDiv = $('<div>').addClass('trivia');
         // Create new element for the question
-        let question = $('<h2>').html(game.questions[game.pickedQuestions[0]].ask);
+        let question = $('<h2>').html(game.questions[game.pickedQuestions[game.currentQuestion]].ask);
         // append the element to the new div
         newDiv.append(question);
         // Show question on the DOM
         $('.main-content').append(newDiv);
         // For the question picked, show answers.
         // Add wrong answer to questionAnswers Array.
-        for (j = 0; j < game.questions[game.pickedQuestions[0]].wrongAnswer.length; j++) {
-            game.questionAnswers.push(game.questions[game.pickedQuestions[0]].wrongAnswer[j]);
+        for (j = 0; j < game.questions[game.pickedQuestions[game.currentQuestion]].wrongAnswer.length; j++) {
+            game.questionAnswers.push(game.questions[game.pickedQuestions[game.currentQuestion]].wrongAnswer[j]);
         }
-        // Add class to know which answer is correct
-        let answer = $('<button>').addClass('btn-answers correct').html(game.questions[game.pickedQuestions[0]].correctAnswer);
         // Make sure correct answer is in different positions in the array.
         let newPosition = Math.floor(Math.random() * (game.questionAnswers.length));
         // Add correct answer to questionAnswers array.
-        game.questionAnswers.splice(newPosition, 0, (answer));
+        game.questionAnswers.splice(newPosition, 0, (game.questions[game.pickedQuestions[game.currentQuestion]].correctAnswer));
         console.log(game.questionAnswers); // Delete Later
         // Show answers on the DOM
         for (i = 0; i < game.questionAnswers.length; i++) {
+            // Add separate class to distiguish the correct answer
             if (i == newPosition) {
-                $('.main-content').append(answer);
+                $('.main-content').append($('<button>').addClass('btn-answers correct').html(game.questionAnswers[i]));
             } else {
-                $('.main-content').append($('<button>').addClass('btn-answers').html(game.questionAnswers[i]));
+                $('.main-content').append($('<button>').addClass('btn-answers wrong').html(game.questionAnswers[i]));
             }
         }
+    },
+
+    // What happens when answer picked is correct
+    correct: function () {
+        $('.main-content').empty();
+        $('.main-content').append($('<h2>').html('You are correct!'));
+        $('.main-content').append($('<p>').html(`Answer is: ${game.questions[game.pickedQuestions[game.currentQuestion]].correctAnswer}`));
+        game.wins++;
+        game.currentQuestion++;
+        setTimeout(game.showQuestions, 3000);
+    },
+
+    // What happens when answer picked is wrong
+    wrong: function () {
+        $('.main-content').empty();
+        $('.main-content').append($('<h2>').html('You are wrong!'));
+        $('.main-content').append($('<p>').html(`Answer is: ${game.questions[game.pickedQuestions[game.currentQuestion]].correctAnswer}`));
+        game.losses++;
+        game.currentQuestion++;
+        setTimeout(game.showQuestions, 3000);
     }
 }
 
@@ -110,12 +147,15 @@ $(document).ready(function () {
         // Check if target is correct
         if (target.is('.correct')) {
             console.log('You are correct');
-        } else {
+            game.correct();
+        }
+        if (target.is('.wrong')) {
             console.log('Wrong!');
+            game.wrong();
         }
     })
 })
 game.getQuestions();
 game.showQuestions();
-console.log(game.questions[game.pickedQuestions[0]].ask);
+console.log(game.questions[game.pickedQuestions[game.currentQuestion]].ask);
 console.log(game.pickedQuestions);
